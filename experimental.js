9df5581403867a41571b04ad1f440993817c7f9a
Pavelosky <pawel.bialkowskigru@gmail.com>
2024-03-09 16:10:33 +0100

    Slider class added

--- experimental.js
+++ experimental.js
@@ -1,29 +1,30 @@
 let img;
 let treshold = 20
 
-function preload() {
-    img = loadImage('image.png');
-}
+class Slider {
+    constructor(x, y, initialValue) {
+        this.slider = createSlider(0, 255, initialValue);
+        this.slider.position(x, y);
+        this.value = initialValue;
+    }
 
-function setup() {
-    createCanvas(1000, 120 * 5);
-    pixelDensity(1);
-
-
-    noStroke();
+    getValue() {
+        return this.slider.value();
+    }
 }
 
 class ImageProcessor {
-    constructor(image, x, y, func) {
+    constructor(image, x, y, func, slider) {
         this.image = image;
         this.x = x;
         this.y = y;
-        this.func = func;
+        this.func = func
+        this.slider = slider;
     }
 
     processImage() {
         // Call the processing function and store the processed image
-        this.image = this.func();
+        this.image = this.func(this.image, this.slider.getValue());
     }
 
     draw() {
@@ -31,6 +32,23 @@ class ImageProcessor {
         image(this.image, this.x, this.y);
     }
 }
+
+function preload() {
+    img = loadImage('image.png');
+}
+
+function setup() {
+    createCanvas(1000, 120 * 5);
+    pixelDensity(1);
+    // Create sliders for each image
+    slider1 = new Slider(20, 20, 20);
+    slider2 = new Slider(20, 120, 20);
+    slider3 = new Slider(20, 220, 20);
+
+    noStroke();
+}
+
+
 
 // Convert image to grayscale and increase brightness by 20%
 function grayscale(){
@@ -109,54 +127,43 @@ function greenPixelsOnly(){
     return image;
 }
 
-function redTreshold(){
-    
-    let image = createImage(img.width, img.height);
-    image.copy(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
-    image.loadPixels();
+function redTreshold(image, treshold){
+    let processedImage = createImage(image.width, image.height);
+    processedImage.copy(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
+    processedImage.loadPixels();
 
-    tresholdSlider = createSlider(0, 255, 20);
-    tresholdSlider.position(20, 20);
-    
 
-    treshold = tresholdSlider.value();
 
-    for (let i = 0; i < image.pixels.length; i += 4) {
-        let r = image.pixels[i];
-        let g = image.pixels[i + 1];
-        let b = image.pixels[i + 2];
+    for (let i = 0; i < processedImage.pixels.length; i += 4) {
+        let r = processedImage.pixels[i];
+        let g = processedImage.pixels[i + 1];
+        let b = processedImage.pixels[i + 2];
 
-        image.pixels[i] = r;
-        image.pixels[i + 1] = treshold;
-        image.pixels[i + 2] = b;
+        processedImage.pixels[i] = r;
+        processedImage.pixels[i + 1] = treshold;
+        processedImage.pixels[i + 2] = b;
     }
-    image.updatePixels();
-    return image;
+    processedImage.updatePixels();
+    return processedImage;
 }
 
 
-function blueTreshold(){
-    
-    let image = createImage(img.width, img.height);
-    image.copy(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
-    image.loadPixels();
+function blueTreshold(image, treshold){
+    let processedImage = createImage(image.width, image.height);
+    processedImage.copy(image, 0, 0, image.width, image.height, 0, 0, image.width, image.height);
+    processedImage.loadPixels();
 
-    tresholdSlider = createSlider(0, 255, 20);
-    tresholdSlider.position(80, 80);
-    
-    treshold = tresholdSlider.value();
+    for (let i = 0; i < processedImage.pixels.length; i += 4) {
+        let r = processedImage.pixels[i];
+        let g = processedImage.pixels[i + 1];
+        let b = processedImage.pixels[i + 2];
 
-    for (let i = 0; i < image.pixels.length; i += 4) {
-        let r = image.pixels[i];
-        let g = image.pixels[i + 1];
-        let b = image.pixels[i + 2];
-
-        image.pixels[i] = r;
-        image.pixels[i + 1] = g;
-        image.pixels[i + 2] = treshold;
+        processedImage.pixels[i] = r;
+        processedImage.pixels[i + 1] = g;
+        processedImage.pixels[i + 2] = treshold;
     }
-    image.updatePixels();
-    return image;
+    processedImage.updatePixels();
+    return processedImage;
 }
 
 
@@ -166,8 +173,8 @@ function draw() {
     image(img, 0, 0);
 
     // Create instances of ImageProcessor
-    const imageProcessor = new ImageProcessor(img, 0, 0, redTreshold);
-    const blueProcessor = new ImageProcessor(img, 200, 200, blueTreshold);
+    const imageProcessor = new ImageProcessor(img, 0, 0, redTreshold, slider1);
+    const blueProcessor = new ImageProcessor(img, 200, 200, blueTreshold, slider2);
     
 
     // Process the images
