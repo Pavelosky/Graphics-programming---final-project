427c13f731c6de457470d1f22c441464c0176776
Pavelosky <pawel.bialkowskigru@gmail.com>
2024-03-09 21:17:24 +0100

    final_project almost done

--- experimental.js
+++ experimental.js
@@ -1,5 +1,14 @@
 let img;
 let treshold = 20
+let faceapi;
+let detections;
+let faceDetection;
+
+// by default all options are set to true
+const detectionOptions = {
+    withLandmarks: true,
+    withDescriptors: false,
+};
 
 class Slider {
     constructor(x, y, initialValue) {
@@ -14,12 +23,11 @@ class Slider {
 }
 
 class ImageProcessor {
-    constructor(image, x, y, func, slider) {
+    constructor(image, x, y, func) {
         this.image = image;
         this.x = x;
         this.y = y;
         this.func = func;
-        this.slider = slider;
     }
 
     processImage() {
@@ -54,6 +62,63 @@ class ImageProcessor {
     }
 }
 
+class FaceDetection {
+    constructor() {
+        this.faceapi = ml5.faceApi(detectionOptions, this.modelReady.bind(this));
+    }
+
+    setup() {
+        createCanvas(600, 800);
+        background(220);
+        textAlign(RIGHT);
+    }
+
+    modelReady() {
+        console.log("ready!");
+        console.log(this.faceapi);
+        this.faceapi.detectSingle(img, this.gotResults.bind(this));
+    }
+
+    gotResults(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        // console.log(result)
+        this.detections = result;
+
+        // background(220);
+        background(255);
+        image(img, 0, 0, img.width, img.height);
+        if (this.detections) {
+            // console.log(detections)
+            this.drawBox(this.detections);
+            this.drawLandmarks(this.detections);
+        }
+    }
+
+    drawBox(detections) {
+        const alignedRect = detections.alignedRect;
+        const { _x, _y, _width, _height } = alignedRect._box;
+        noFill();
+        stroke(161, 95, 251);
+        strokeWeight(2);
+        rect(_x, _y, _width, _height);
+    }
+
+    drawLandmarks(detections) {
+        noFill();
+        stroke(161, 95, 251);
+        strokeWeight(2);
+
+        const landmarks = detections.landmarks._positions;
+
+        for (let i = 0; i < landmarks.length; i++) {
+            point(landmarks[i]._x, landmarks[i]._y);
+        }
+    }
+}
+
 function preload() {
     img = loadImage('image.png');
 }
@@ -65,9 +130,9 @@ function setup() {
     slider1 = new Slider(20, 20, 20);
     // slider2 = new Slider(20, 120, 20);
     // slider3 = new Slider(20, 220, 20);
+    faceDetection = new FaceDetection();
     noStroke();
 }
-
 
 
 // Convert image to grayscale and increase brightness by 20%
@@ -168,7 +233,7 @@ function draw() {
     image(img, 0, 0);
 
     // Create instances of ImageProcessor
-    const imageProcessor = new ImageProcessor(img, 0, 0, rgbToHsv);
+    const imageProcessor = new ImageProcessor(img, 0, 0, grayscale);
     // const blueProcessor = new ImageProcessor(img, 200, 200, blueTreshold, slider2);
     
 
--- face_detection.js
+++ face_detection.js
@@ -1,6 +1,7 @@
 let faceapi;
 let img;
 let detections;
+let faceDetection;
 
 // by default all options are set to true
 const detectionOptions = {
@@ -12,55 +13,68 @@ function preload() {
     img = loadImage('image.png');
 }
 
+class FaceDetection {
+    constructor() {
+        this.faceapi = ml5.faceApi(detectionOptions, this.modelReady.bind(this));
+    }
+
+    setup() {
+        createCanvas(600, 800);
+        background(220);
+        textAlign(RIGHT);
+    }
+
+    modelReady() {
+        console.log("ready!");
+        console.log(this.faceapi);
+        this.faceapi.detectSingle(img, this.gotResults.bind(this));
+    }
+
+    gotResults(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        // console.log(result)
+        this.detections = result;
+
+        // background(220);
+        background(255);
+        image(img, 0, 0, img.width, img.height);
+        if (this.detections) {
+            // console.log(detections)
+            this.drawBox(this.detections);
+            this.drawLandmarks(this.detections);
+        }
+    }
+
+    drawBox(detections) {
+        const alignedRect = detections.alignedRect;
+        const { _x, _y, _width, _height } = alignedRect._box;
+        noFill();
+        stroke(161, 95, 251);
+        strokeWeight(2);
+        rect(_x, _y, _width, _height);
+    }
+
+    drawLandmarks(detections) {
+        noFill();
+        stroke(161, 95, 251);
+        strokeWeight(2);
+
+        const landmarks = detections.landmarks._positions;
+
+        for (let i = 0; i < landmarks.length; i++) {
+            point(landmarks[i]._x, landmarks[i]._y);
+        }
+    }
+}
+
+
+
 function setup() {
     createCanvas(600, 800);
     background(220);
-
-    faceapi = ml5.faceApi(detectionOptions, modelReady);
-    textAlign(RIGHT);
-}
-
-function modelReady() {
-    console.log("ready!");
-    console.log(faceapi);
-    faceapi.detectSingle(img, gotResults);
-}
-
-function gotResults(err, result) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    // console.log(result)
-    detections = result;
-
-    // background(220);
-    background(255);
-    image(img, 0, 0, img.width, img.height);
-    if (detections) {
-        // console.log(detections)
-        drawBox(detections);
-        drawLandmarks(detections);
-    }
-}
-
-function drawBox(detections) {
-    const alignedRect = detections.alignedRect;
-    const { _x, _y, _width, _height } = alignedRect._box;
-    noFill();
-    stroke(161, 95, 251);
-    strokeWeight(2);
-    rect(_x, _y, _width, _height);
-}
-
-function drawLandmarks(detections) {
-    noFill();
-    stroke(161, 95, 251);
-    strokeWeight(2);
-
-    const landmarks = detections.landmarks._positions;
-
-    for (let i = 0; i < landmarks.length; i++) {
-        point(landmarks[i]._x, landmarks[i]._y);
-    }
+    faceDetection = new FaceDetection();
+    faceDetection.setup();
 }
