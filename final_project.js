48ba4b4d07a37972fbbccfd772d507fd8853e618
Pavelosky <pawel.bialkowskigru@gmail.com>
2024-03-11 07:20:10 +0100

    added comments

--- final_project.js
+++ final_project.js
@@ -1,21 +1,29 @@
+// initialize the video and the variables to control the mode of the application
 let video;
 
+// Variables to control the mode of the application
 let snapshotMode = false;
 let showCamera = true;
-let snapshotTaken = false;
 
+// Treshold for the red, green and blue channels
 let treshold = 20
+
+// Variables for the face detection
 let faceapi;
 let detections;
 let faceDetection;
+
+// Buffer image to store the snapshot
 let bufferImage
 
+// Sliders to manipulate the image
 let sliderRed;
 let sliderGreen;
 let sliderBlue;
 let sliderYCbCr;
 let sliderHSV;
 
+// Array of functions to process the images
 let imageLabels = [
     original,
     grayscale,
@@ -31,14 +39,14 @@ let imageLabels = [
     rgbToHsv
 ];
 
-let processedImages = []
+let processedImages  = []
 
 // by default all options are set to true
 const detectionOptions = {
-    withLandmarks: true,
     withDescriptors: false,
 };
 
+// Slider class to create the sliders
 class Slider {
     constructor(x, y, initialValue) {
         this.slider = createSlider(0, 255, initialValue);
@@ -49,8 +57,9 @@ class Slider {
     getValue() {
         return this.slider.value();
     }
-}
+};
 
+// ImageProcessor class to process the images
 class ImageProcessor {
     constructor(image, x, y, func) {
         this.image = image;
@@ -58,7 +67,7 @@ class ImageProcessor {
         this.y = y;
         this.func = func;
     }
-
+    // Process the image by passing each pixel to the function
     processImage() {
         let processedImage = createImage(this.image.width, this.image.height);
         processedImage.copy(this.image, 0, 0, this.image.width, this.image.height, 0, 0, this.image.width, this.image.height);
@@ -85,6 +94,7 @@ class ImageProcessor {
     }
 }
 
+// FaceDetection class to detect the face in the image - this class is based on the example provided by ml5.js with some modifications needed to fit the project
 class FaceDetection {
     constructor(bufferImage, x, y) {
         this.x = x;
@@ -117,10 +127,10 @@ class FaceDetection {
     drawBox(detections) {
         const alignedRect = detections.alignedRect;
         const { _x, _y, _width, _height } = alignedRect._box;
-
+        
         // Center of the face detected in the new image position
         const centerX = this.x + _x + _width / 2;
-        const centerY = this.y + _y + _height / 2;
+        const centerY = this.y + _y + _height / 2;  
 
         // Calculate the width and height of the box based on the detected face
         const boxWidth = _width * 1.2; // Adjusting the width of the box
@@ -134,23 +144,26 @@ class FaceDetection {
     }
 }
 
-
-// Convert image to grayscale and increase brightness by 20%
+// Original image - it returns the original values of the pixels
 function original(r, g, b) {
-
     return [r, g, b];
 }
 
+// Convert image to grayscale and increase brightness by 20%
 function grayscale(r, g, b) {
+    // calculate the average of the three channels
     let gray = (r + g + b) / 3;
-    // this limits the brightness to 255 (point 5 in the assignment)
+
+    // limit the brightness to 255 (point 5 in the assignment)
     let brightenedGray = min(gray * 1.2, 255);
+
     let red = brightenedGray;
     let green = brightenedGray;
     let blue = brightenedGray;
     return [red, green, blue];
 }
 
+// Produce black image
 function black(r, g, b) {
     let red = 0;
     let green = 0;
@@ -158,6 +171,7 @@ function black(r, g, b) {
     return [red, green, blue];
 }
 
+// Show only red pixels
 function redPixelsOnly(r, g, b) {
     let red = r;
     let green = 0;
@@ -165,6 +179,7 @@ function redPixelsOnly(r, g, b) {
     return [red, green, blue];
 }
 
+// Show only green pixels
 function greenPixelsOnly(r, g, b) {
     let red = 0;
     let green = g;
@@ -172,6 +187,7 @@ function greenPixelsOnly(r, g, b) {
     return [red, green, blue];
 }
 
+// Show only blue pixels
 function bluePixelsOnly(r, g, b) {
     let red = 0;
     let green = 0;
@@ -179,6 +195,10 @@ function bluePixelsOnly(r, g, b) {
     return [red, green, blue];
 }
 
+// Manipulate the red channel - this is ther reverse of the redPixelsOnly function. I actualle expectected something else, but it makes sense 
+// to me now that I think about it. When only red channel is shown, the other channels are set to 0, so we see only red pixels at their original value.
+// When we manipulate the red channel, we set the other channels to their original value and the red channel to the value of the slider, therefore 
+// we can either remove the red pixels or make the image more red. - The same applies to the green and blue channels
 function redTreshold(r, g, b) {
     treshold = sliderRed.getValue();
     let red = treshold;
@@ -187,6 +207,7 @@ function redTreshold(r, g, b) {
     return [red, green, blue];
 }
 
+// Manipulate the green channel
 function greenTreshold(r, g, b) {
     treshold = sliderGreen.getValue();
     let red = r;
@@ -195,6 +216,7 @@ function greenTreshold(r, g, b) {
     return [red, green, blue];
 }
 
+// Manipulate the blue channel
 function blueTreshold(r, g, b) {
     treshold = sliderBlue.getValue();
     let red = r;
@@ -203,6 +225,10 @@ function blueTreshold(r, g, b) {
     return [red, green, blue];
 }
 
+// Make the colorspace conversion to YCbCr. This one I find the most fascinating and I can't quite figure it out.
+// I was able to apply the math to the pixels and get the results, but it's hard for me to grasp the way the colors change in the image.
+// As you move the slider, the image changes in a very unexpected way. On right end it resembles the image where blue pixels are removed, 
+// but as you move the slider to the right, the image becomes to look unnatural. I think it's because the YCbCr color space is not as intuitive as RGB.
 function rgbToYCbCr(r, g, b) {
     let y = 0.299 * r + 0.587 * g + 0.114 * b * sliderYCbCr.getValue() / 100;
     let cb = 128 - 0.168736 * r - 0.331264 * g + 0.5 * b * sliderYCbCr.getValue() / 100;
@@ -241,24 +267,25 @@ function rgbToHsv(r, g, b) {
 }
 
 function setup() {
-    createCanvas(640, 120 * 5);
+    createCanvas(160 * 3, 120 * 5);
     pixelDensity(1)
+
+    // Create a video element
     video = createCapture(VIDEO);;
     video.hide()
     noStroke()
-
 }
 
 function draw() {
     background(0);
-
+    // Mode to show the camera and take a snapshot
     if (showCamera) {
-        image(video, 0, 0)
+        image(video, 0, 0, video.width * 2/3, video.height * 2/3);
         textAlign(CENTER);
         fill(255)
-        text("Click your mouse to take a snapshot", width / 2, height / 2, 100, 100)
+        text("Click your mouse to take a snapshot", width/2, height/2, 100, 100)
     }
-
+    // Mode to show the snapshot and the processed images
     if (snapshotMode) {
         bufferImage.resize(160, 120)
 
@@ -268,34 +295,37 @@ function draw() {
 
         faceDetection.gotResults(null, faceDetection.detections);
     }
-
 }
 
+// function to take a snapshot of the video
 function mouseClicked() {
-    if (!snapshotTaken) {
+    if(!snapshotMode){
         // Take a snapshot of the video
         bufferImage = createImage(video.width, video.height);
         bufferImage.copy(video, 0, 0, video.width, video.height, 0, 0, video.width, video.height);
 
+        // Change the mode to show the snapshot and the processed images
         showCamera = false;
         snapshotMode = true;
-        snapshotTaken = true;
 
+        // Create the processed images
         for (let i = 0; i < imageLabels.length; i++) {
             let x = 160 * (i % 3);
             let y = 120 * int(i / 3);
 
+            // Create the image processor object
             const imageProcessor = new ImageProcessor(bufferImage, x, y, imageLabels[i]);
             processedImages.push(imageProcessor);
         }
-
+        
+        // Create the face detection object
         faceDetection = new FaceDetection(bufferImage, 0, 480);
+
+        // Create the sliders
         sliderRed = new Slider(20, 330, 20);
         sliderGreen = new Slider(180, 330, 20);
         sliderBlue = new Slider(340, 330, 20);
         sliderYCbCr = new Slider(180, 450, 20);
         sliderHSV = new Slider(340, 450, 20);
-
     }
-
 }
